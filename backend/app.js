0424ef32ee228cd1fdb28dc8edf9e121f81c81b2
Ronit Bose <147628969+rbose3@users.noreply.github.com>
2025-04-21 11:55:43 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
 const app = express();
-config({ path: "./config/config.env" });
+config();
 
 app.use(
   cors({
